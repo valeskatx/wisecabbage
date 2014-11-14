68c5850f52bb84a253adee5e26d0417d0c50ed8e
Valeska Basilio <valeskatx@gmail.com>
2014-11-13 23:33:53 -0600

    Add images to home page

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
-
+$(document).foundation({
+  orbit: {
+    animation: 'slide',
+    timer_speed: 3000,
+    pause_on_hover: true,
+    animation_speed: 1000,
+    navigation_arrows: true,
+    bullets: false
+  }
+});
